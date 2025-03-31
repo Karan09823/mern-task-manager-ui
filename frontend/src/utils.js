ec6d81347209d0c1938d2ea22df97b670cd8a174
Karan Kumar  Sahu <102219640+Karan09823@users.noreply.github.com>
2025-03-31 23:56:00 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
   toast[type](message);
 };
 
-export const API_URL = "https://mern-task-manager-api-beta.vercel.app/";
+export const API_URL = "https://mern-task-manager-api-beta.vercel.app";
