136496d98162b01fb95f506462e8afc335e43aef
Karan09823 <karankumar514418@gmail.com>
2025-03-31 23:41:47 +0530

    task-manager-ui

---
+++ frontend/src/utils.js
@@ -0,0 +1,7 @@
+import { toast } from "react-toastify";
+
+export const notify = (message, type) => {
+  toast[type](message);
+};
+
+export const API_URL = "https://mern-task-manager-api-beta.vercel.app/";
